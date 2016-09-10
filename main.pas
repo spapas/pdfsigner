9693ece94621dfb88fa3aadbfe05dfe4501467a4
Serafeim Papastefanos <spapas@gmail.com>
2016-09-10 23:57:28 +0300

    Some changes

--- main.pas
+++ main.pas
@@ -5,22 +5,26 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls, Process;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    GroupBox1: TGroupBox;
+    signPdfsButton: TButton;
     fileBrowseButton: TButton;
     folderEdit: TEdit;
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
+    pdfDescriptionLabel: TLabel;
     selectFolderDialog: TSelectDirectoryDialog;
     pdfFiles: TStringList;
     procedure fileBrowseButtonClick(Sender: TObject);
     procedure findFiles();
+    procedure signPdfsButtonClick(Sender: TObject);
   private
     { private declarations }
     folder: String;
@@ -51,13 +55,35 @@ end;
 
 procedure TForm1.findFiles();
 begin
-      pdfFiles := FindAllFiles(folder, '*.pas;*.pp;*.p;*.*', False);
+      pdfFiles := FindAllFiles(folder, '*.pdf;*.*', False);
       try
-          showmessage(Format('Found %d Pascal source files',[pdfFiles.Count]));
+          //showmessage(Format('Found %d pdf files',[pdfFiles.Count]));
+          pdfDescriptionLabel.Caption:=Format('Found %d pdf files',[pdfFiles.Count]);
+          if pdfFiles.Count > 0 then
+          begin
+               signPdfsButton.Enabled:=true;
+          end
       finally
           pdfFiles.Free;
       end;
 end;
+
+procedure TForm1.signPdfsButtonClick(Sender: TObject);
+var s: ansistring;
+begin
+    showmessage('Will try to run JSignPDF to sign all pdfs of folder ' + folder +'...');
+
+    if RunCommand('D:\Util\curl.exe', ['www.google.com'], s) then
+    begin
+         showmessage('Success!');
+         showmessage(s) ;
+    end
+    else
+    begin
+        showmessage('Error while trying to run JSignPDF') ;
+    end;
+end;
+
 
 end.
 
